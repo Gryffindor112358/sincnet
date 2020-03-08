920e8ff7e8ca3e91223692269c0aed6f2c43021e
YuHuai <1286244842@qq.com>
2020-03-08 10:52:49 +0800

    标签的来源

---
+++ load_lable.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+test = np.load(r'D:\Equipments\PyCharm Community Edition 2019.1.3\Projects\all_needed\sincnet\sincnet\data_lists\TIMIT_labels.npy')
+print(test)   # 在data_list文件夹中的labels中存储的就是对应加载数据的标签
